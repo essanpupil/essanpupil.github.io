ef407eb21b7374afa3baa04a601f8d971f4793c2
Ikhsan N. Rosyidin <essanpupil@gmail.com>
2026-03-29 14:13:13 +0700

    fix missing assets caused by base url

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1774767710',resources: [
+    cacheName: 'chirpy-1774768283',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
