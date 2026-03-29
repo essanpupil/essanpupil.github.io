c41cfc8ea5002b2bf3e2769ccff6a745752b5d26
Ikhsan N. Rosyidin <essanpupil@gmail.com>
2026-03-29 13:51:47 +0700

    change image profile

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1774766077',resources: [
+    cacheName: 'chirpy-1774767069',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
