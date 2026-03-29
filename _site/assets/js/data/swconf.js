59695a8d63aa439093616da2a79b368fc4fcbfef
Ikhsan N. Rosyidin <essanpupil@gmail.com>
2026-03-29 14:17:14 +0700

    add missing gem

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1774768283',resources: [
+    cacheName: 'chirpy-1774768567',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
